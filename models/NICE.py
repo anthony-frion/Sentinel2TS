845946cdcfcd2e76c282b3aa2e9df4b3f4f08010
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2025-01-20 09:26:42 +0100

    Update NICE.py
    add 'even_odd' option

--- models/NICE.py
+++ models/NICE.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class NICE(nn.Module) :
 
-  def __init__(self, d, k, hidden, base_dist):
+  def __init__(self, d, k, hidden, base_dist, even_odd=False):
     super().__init__()
     self.d, self.k = d, k
     self.net = nn.Sequential(
@@ -12,10 +12,13 @@ class NICE(nn.Module) :
         nn.LeakyReLU(),
         nn.Linear(hidden, d - k))
     self.base_dist = base_dist
+    self.even_odd = even_odd
 
   def forward(self, x, flip=False) :
-
-    x1, x2 = x[:, :x.shape[1]//2], x[:, x.shape[1]//2:]
+    if not self.even_odd:
+      x1, x2 = x[:, :x.shape[1]//2], x[:, x.shape[1]//2:]
+    else:
+      x1, x2 = x[:, ::2], x[:, 1::2]
     if flip:
       x2, x1 = x1, x2
     z1, z2 = x1, x2 + self.net(x1)
@@ -37,13 +40,19 @@ class NICE(nn.Module) :
 
     if flip:
       x2, x1 = x1, x2
-    return torch.cat([x1, x2], -1)
+    if not self.even_odd:
+      return torch.cat([x1, x2], -1)
+    else:
+      temp1, temp2 = torch.zeros_like(torch.cat([x1, x2], -1)), torch.zeros_like(torch.cat([x1, x2], -1))
+      temp1[:,::2] = x1
+      temp2[:,1::2] = x2
+      return temp1 + temp2
 
 class stacked_NICE(nn.Module):
-    def __init__(self, d, k, hidden, n, base_dist):
+    def __init__(self, d, k, hidden, n, base_dist, even_odd=False):
         super().__init__()
         self.bijectors = nn.ModuleList([
-            NICE(d, k+((i%2)*(d%2)), hidden=hidden, base_dist=base_dist) for i in range(n)
+            NICE(d, k+((i%2)*(d%2)), hidden=hidden, base_dist=base_dist, even_odd=even_odd) for i in range(n)
         ])
         self.flips = [True if i%2 else False for i in range(n)]
         self.base_dist = base_dist
